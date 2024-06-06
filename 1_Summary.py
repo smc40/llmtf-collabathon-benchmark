6b5f11ba0228613e82ad48351c612f9698028627
alho94 <alexander.horst@swissmedic.ch>
2024-06-06 10:17:36 +0200

    a lot of messy changes

--- 1_Summary.py
+++ 1_Summary.py
@@ -2,35 +2,47 @@ import streamlit as st
 import pandas as pd
 import json
 import random
+import os
 from time import time
 from rouge_score import rouge_scorer
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 import nltk
-
-import os
 from openai import AzureOpenAI
-
 import spacy
+import time as times
 
 nlp = spacy.load("en_core_web_sm")
-
-# # Download NLTK data
-# nltk.download("punkt")
 
 # Set the mode (either "dummy" or "real")
 MODE = "real"
 models = ["gpt-4", "gpt-35-turbo"]
 
-
 st.set_page_config(layout="wide")
+
+# Streamlit application
+st.title("Benchmark your LLM on creating a Summary")
+st.write("PoC build during the Collabathon of the LLM Taskforce 2024 in Bern")
+
+
+# Create data directory if it doesn't exist
+if not os.path.exists("./data"):
+    os.makedirs("./data")
+
+df = pd.DataFrame()
+
+
+def exitster(filename="./data/results.json"):
+    if os.path.exists(filename):
+        return True
+    return False
+
+
+show = exitster()
 
 
 @st.cache_data
 def generate_summary(
-    full_text: str,
-    selected_models: str,
-    user_prompt: str,
-    system_prompt: str = "You are a helpful assistant.",
+    full_text, selected_model, user_prompt, system_prompt="You are a helpful assistant."
 ):
     client = AzureOpenAI(
         azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
@@ -39,7 +51,7 @@ def generate_summary(
     )
 
     response = client.chat.completions.create(
-        model=selected_models,  # model = "gpt-35-turbo" or "gpt-4".
+        model=selected_model,
         messages=[
             {
                 "role": "system",
@@ -52,7 +64,6 @@ def generate_summary(
     return response.choices[0].message.content
 
 
-# Define a function to validate the JSON structure
 @st.cache_data
 def validate_json_structure(data):
     if not isinstance(data, list):
@@ -65,14 +76,12 @@ def validate_json_structure(data):
     return True
 
 
-# Function to fake a summary prediction in dummy mode
 def dummy_summary_prediction(summary):
     words = summary.split()
     random.shuffle(words)
     return " ".join(words)
 
 
-# Function to calculate ROUGE and BLEU scores
 def calculate_scores(reference, prediction):
     scorer = rouge_scorer.RougeScorer(["rouge1", "rougeL"], use_stemmer=True)
     rouge_scores = scorer.score(reference, prediction)
@@ -89,41 +98,24 @@ def calculate_scores(reference, prediction):
 @st.cache_data
 def process_uploaded_file(uploaded_file):
     data = json.load(uploaded_file)
-    if validate_json_structure(data):
-        df = pd.DataFrame(data)
-        return df
-    else:
-        return None
+    df = pd.DataFrame(data)
+    return df
+    # if validate_json_structure(data):
+    #     df = pd.DataFrame(data)
+    #     return df
+    # else:
+    #     return pd.DataFrame()
 
 
-# Streamlit application
-st.title("Benchmark your LLM on creating a Summary")
-st.write("PoC build during the Collabathon of the LLM Taskforce 2024 in Bern")
-
-default_user_prompt = st.text_area(
-    "Specify the prompt you want to use (if not provided in the JSON)",
-    value="Summarize this document to a max of 2000 words",
-)
-
-selected_model = st.radio("Select model to benchmark 📈", models)
-
-st.write(
-    "Upload a JSON file containing documents with full-text, summary, source, and URL. The tool will benchmark the performance of your LLM in generating summaries."
-)
-
-
-# Function to calculate token length
 def calculate_token_length(text):
     doc = nlp(text)
     return len(doc)
 
 
 def calculate_f1_recall_precision(reference, predicted):
-    # Tokenize the reference and predicted summaries
     ref_tokens = set(reference.split())
     pred_tokens = set(predicted.split())
 
-    # Calculate precision, recall, and F1 score
     precision = (
         len(ref_tokens & pred_tokens) / len(pred_tokens) if len(pred_tokens) > 0 else 0
     )
@@ -139,29 +131,43 @@ def calculate_f1_recall_precision(reference, predicted):
     return f1, recall, precision
 
 
+def save_results_to_file(results, filename="./data/results.json"):
+    results.to_json(filename, orient="records")
+
+
+def load_results_from_file(filename="./data/results.json"):
+    if os.path.exists(filename):
+        return pd.read_json(filename)
+    return pd.DataFrame()
+
+
 uploaded_file = st.file_uploader("Upload JSON File", type="json")
+
+df = load_results_from_file()
 
 if uploaded_file:
     df = process_uploaded_file(uploaded_file)
-    if df is not None:
+    if not df.empty:
+        default_user_prompt = st.text_area(
+            "Specify the prompt you want to use (if not provided in the JSON)",
+            value="Summarize this document to a max of 2000 words",
+        )
 
-        # Insights per source
+        selected_model = st.radio("Select model to benchmark 📈", models)
+
         insights = df["source"].value_counts().to_frame().reset_index()
         insights.columns = ["Source", "Count"]
         insights["source_flags"] = insights["Source"].map(
-            {"MHRA": "🇬🇧", "EFSA": "🇪🇺", "SMC": "🇨🇭"}
+            {"mhra": "🇬🇧", "EFSA Journal": "🇪🇺", "SMC": "🇨🇭"}
         )
 
-        # Group by the 'source' column
         grouped_df = df.groupby("source")
 
-        # Calculate token length for each summary
         df["tokens_len_full_text"] = df["full-text"].apply(calculate_token_length)
         insights["tokens_len_full_text"] = (
             grouped_df["tokens_len_full_text"].mean().values
         )
 
-        # Calculate token length for each summary
         df["tokens_len_summary"] = df["summary"].apply(calculate_token_length)
         insights["tokens_len_summary"] = grouped_df["tokens_len_summary"].mean().values
 
@@ -182,7 +188,6 @@ if uploaded_file:
                 if MODE == "dummy":
                     predicted = dummy_summary_prediction(row["summary"])
                 else:
-                    # Real mode: call OpenAI API (assuming there's a function for that)
                     prompt = row.get("prompt", default_user_prompt)
                     predicted = generate_summary(
                         row["full-text"], selected_model, prompt
@@ -190,7 +195,6 @@ if uploaded_file:
 
                 summary_predicted.append(predicted)
 
-                # Calculate scores for each row
                 rouge1, rougeL, bleu = calculate_scores(row["summary"], predicted)
                 f1, recall, precision = calculate_f1_recall_precision(
                     row["summary"], predicted
@@ -204,9 +208,7 @@ if uploaded_file:
                 precision_scores.append(precision)
 
             df["summary_predicted"] = summary_predicted
-
-            # Calculate token length for each summary
-            df["tokens_len_summary_predicted"] = df["summary"].apply(
+            df["tokens_len_summary_predicted"] = df["summary_predicted"].apply(
                 calculate_token_length
             )
             insights["tokens_len_summary_predicted"] = (
@@ -219,55 +221,70 @@ if uploaded_file:
             df["F1"] = f1_scores
             df["Recall"] = recall_scores
             df["Precision"] = precision_scores
+            df["selected_model"] = selected_model
 
             end_time = time()
             total_time = end_time - start_time
 
-            # Calculate average scores per source
-            scores = []
-            for source in df["source"].unique():
-                source_data = df[df["source"] == source]
-                avg_rouge1 = source_data["ROUGE-1"].mean()
-                avg_rougeL = source_data["ROUGE-L"].mean()
-                avg_bleu = source_data["BLEU"].mean()
-                avg_recall = source_data["Recall"].mean()
-                avg_precision = source_data["Precision"].mean()
-                avg_f1 = source_data["F1"].mean()
-                scores.append(
-                    {
-                        "Model": selected_model,
-                        "Source": source,
-                        "ROUGE-1": avg_rouge1,
-                        "ROUGE-L": avg_rougeL,
-                        "BLEU": avg_bleu,
-                        "Recall": avg_recall,
-                        "Precision": avg_precision,
-                        "F1": avg_f1,
-                    }
-                )
+            # Rename old results file with timestamp
+            if os.path.exists("./data/results.json"):
+                timestamp = times.strftime("%Y%m%d-%H%M%S")
+                os.rename("./data/results.json", f"./data/_results_{timestamp}.json")
 
-            scores_df = pd.DataFrame(scores)
-            st.write("LLM predictions and scores per row:")
-            st.write(df)
+            save_results_to_file(df)
+            show = True
 
-            st.write("Measures:")
-            st.write(f"It took a total of {total_time} seconds to run the benchmark")
-            st.write(scores_df)
-
-            with st.expander("How does ROUGE score work?"):
-                st.write(
-                    "ROUGE score measures the similarity between the machine-generated summary and the reference summaries using overlapping n-grams, word sequences that appear in both the machine-generated summary and the reference summaries. The most common n-grams used are unigrams, bigrams, and trigrams. ROUGE score calculates the recall of n-grams in the machine-generated summary by comparing them to the reference summaries. ROUGE-N (N-gram) scoring. ROUGE-L (Longest Common Subsequence) scoring"
-                )
-                st.write(
-                    "https://medium.com/@sthanikamsanthosh1994/understanding-bleu-and-rouge-score-for-nlp-evaluation-1ab334ecadcb#:~:text=ROUGE%20score%20measures%20the%20similarity,unigrams%2C%20bigrams%2C%20and%20trigrams."
-                )
-
-            with st.expander("How does BLEU score work?"):
-                st.write(
-                    "BLEU score measures the similarity between the machine-translated text and the reference translations using n-grams, which are contiguous sequences of n words. The most common n-grams used are unigrams (single words), bigrams (two-word sequences), trigrams (three-word sequences), and so on."
-                )
-                st.write(
-                    "https://medium.com/@sthanikamsanthosh1994/understanding-bleu-and-rouge-score-for-nlp-evaluation-1ab334ecadcb#:~:text=ROUGE%20score%20measures%20the%20similarity,unigrams%2C%20bigrams%2C%20and%20trigrams."
-                )
     else:
         st.error("The JSON file does not match the required structure.")
+
+
+try:
+
+    if not df.empty and show:
+
+        scores = []
+        for source in df["source"].unique():
+            source_data = df[df["source"] == source]
+            avg_rouge1 = source_data["ROUGE-1"].median()
+            avg_rougeL = source_data["ROUGE-L"].median()
+            avg_bleu = source_data["BLEU"].median()
+            avg_recall = source_data["Recall"].median()
+            avg_precision = source_data["Precision"].median()
+            avg_f1 = source_data["F1"].median()
+            scores.append(
+                {
+                    "Model": str(source_data["selected_model"].head(1).values[0]),
+                    "Source": source,
+                    "ROUGE-1": avg_rouge1,
+                    "ROUGE-L": avg_rougeL,
+                    "BLEU": avg_bleu,
+                    "Recall": avg_recall,
+                    "Precision": avg_precision,
+                    "F1": avg_f1,
+                }
+            )
+        scores_df = pd.DataFrame(scores)
+
+        st.write("LLM predictions and scores per row:")
+        st.write(df)
+        st.write("Measures:")
+        # st.write(f"It took a total of {total_time} seconds to run the benchmark")
+        st.write(scores_df)
+
+        with st.expander("How does ROUGE score work?"):
+            st.write(
+                "ROUGE score measures the similarity between the machine-generated summary and the reference summaries using overlapping n-grams, word sequences that appear in both the machine-generated summary and the reference summaries. The most common n-grams used are unigrams, bigrams, and trigrams. ROUGE score calculates the recall of n-grams in the machine-generated summary by comparing them to the reference summaries. ROUGE-N (N-gram) scoring. ROUGE-L (Longest Common Subsequence) scoring"
+            )
+            st.write(
+                "https://medium.com/@sthanikamsanthosh1994/understanding-bleu-and-rouge-score-for-nlp-evaluation-1ab334ecadcb#:~:text=ROUGE%20score%20measures%20the%20similarity,unigrams%2C%20bigrams%2C%20and%20trigrams."
+            )
+        with st.expander("How does BLEU score work?"):
+            st.write(
+                "BLEU score measures the similarity between the machine-translated text and the reference translations using n-grams, which are contiguous sequences of n words. The most common n-grams used are unigrams (single words), bigrams (two-word sequences), trigrams (three-word sequences), and so on."
+            )
+            st.write(
+                "https://medium.com/@sthanikamsanthosh1994/understanding-bleu-and-rouge-score-for-nlp-evaluation-1ab334ecadcb#:~:text=ROUGE%20score%20measures%20the%20similarity,unigrams%2C%20bigrams%2C%20and%20trigrams."
+            )
+
+except KeyError as e:
+    print(f"received the following error: {e}")
