b9734fb10535f3b03f7f4bc1ccbb281622989baf
Mondaydome <dominic.michel@yahoo.de>
2024-06-13 13:48:59 +0200

    removed punctuation

--- pages/2_Next_Word_Prediction.py
+++ pages/2_Next_Word_Prediction.py
@@ -4,15 +4,21 @@ import json
 import os
 from openai import AzureOpenAI, OpenAI
 from dotenv import load_dotenv
+import string
 
 load_dotenv()
 
-# Azure OpenAI Variables
+
+# Create Azure OpenAI Client
 # client = AzureOpenAI(
 #   azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
 #   api_key = os.getenv("AZURE_OPENAI_API_KEY"),
 #   api_version = "2023-12-01-preview"
 # )
+
+
+
+# Create OpenAI Variables
 client = OpenAI(
    api_key = os.environ.get("OPENAI_API_KEY")
 )
@@ -21,6 +27,7 @@ client = OpenAI(
 # import data for next word prediction
 with open("eval_next_word_efsa.json", 'r') as file:
     df = pd.read_json(file)
+
 
 
 # get the next word prediction by submitting the prefix and then calling the openai api
@@ -37,10 +44,16 @@ def get_next_word_predictions(prefix: str):
     )
     return next_word_predictions
 
+
+
 # compare the predicted next words to the golden next word by submittng predictions and gold standard
 def evaluate_next_words(next_word_predictions, next_word: str):
-    all_predicted_next_words = set([ choice.message.content.split()[0] for choice in next_word_predictions.choices])
+    translator = str.maketrans('', '', string.punctuation) # translator that maps all punctuation to non
+    
+    all_predicted_next_words = set([ choice.message.content.split()[0].translate(translator) for choice in next_word_predictions.choices])
+
     st.write(all_predicted_next_words)
+    
     if next_word in all_predicted_next_words:
         return 1
     else: 
